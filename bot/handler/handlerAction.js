7cbba9308cb1e827def1ab0f4a46b1e001746a21
asif-evan <mohammadevanvai786@gmail.com>
2025-07-27 15:54:49 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,8 +72,8 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "🤖"){
-  if(event.userID == "61577983130441"){
+        if(event.reaction == "🖕"){
+  if(event.userID == "61558166309783"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -82,8 +82,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "😠"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61577716215531"){
+        if(event.reaction == "😡"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61558166309783"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
