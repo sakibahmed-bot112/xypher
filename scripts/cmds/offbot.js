8cb63b8e15ac48db2096ed46a01f26168ff3a69d
asif-evan <mohammadevanvai786@gmail.com>
2025-07-30 20:33:25 +0600

    Update offbot.js

--- scripts/cmds/offbot.js
+++ scripts/cmds/offbot.js
@@ -11,5 +11,9 @@ module.exports = {
 		guide: "{p}{n}"
 	},
 	onStart: async function ({event, api}) {
+		const permission = ["61572589774495","61558166309783","61558166309783"];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("- Paku, You don't have permission to use this command. 🐤", event.threadID, event.messageID);
+    }
 		api.sendMessage("╔════ஜ۩۞۩ஜ═══╗\n\n📴Successfully Turned Off Archives System ✅\n╚════ஜ۩۞۩ஜ═══╝",event.threadID, () =>process.exit(0))}
 };
