afbc218c125d29bf0e4e2ac099a418cd7ee004c8
asif-evan <mohammadevanvai786@gmail.com>
2025-07-31 01:15:48 +0600

    Create left.js

---
+++ scripts/cmds/left.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const request = require("request");
+module.exports = {
+	config: {
+		name: "left",
+		aliases: ["left"],
+		version: "1.0",
+		author: "Sandy",
+		countDown: 5,
+		role: 2,
+		shortDescription: "bot leave the group",
+		longDescription: "",
+		category: "admin",
+		guide: {
+			vi: "{pn} [tid,blank]",
+			en: "{pn} [tid,blank]"
+		}
+	},
+
+	onStart: async function ({ api,event,args, message }) {
+    const permission = ["61572589774495","61558166309783","61558166309783"];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("- 𝗞𝗮𝗻𝗸𝗶𝗿 𝗰𝗲𝗹𝗲 𝗮𝗺𝗮𝗸𝗲 𝗯𝗲𝗿 𝗸𝗼𝗿𝗮𝗿 𝘁𝘂𝗶 𝗸𝗲..!🐤", event.threadID, event.messageID);
+    }
+ var id;
+ if (!args.join(" ")) {
+ id = event.threadID;
+ } else {
+ id = parseInt(args.join(" "));
+ }
+ return api.sendMessage('- তর হোডার গ্রুপে না থাকলে, আমার বাল ছিরা গেলো..!😼', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
+		}
+	};
