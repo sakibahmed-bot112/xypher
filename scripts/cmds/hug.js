b65b05dfbc742e6cdc64ca8ce61609a501c86a98
asifthebot <botchudibyh12@gmail.com>
2025-08-31 12:43:16 +0600

    Update hug.js

--- scripts/cmds/hug.js
+++ scripts/cmds/hug.js
@@ -1,103 +1,86 @@
+const fs = require("fs");
+const path = require("path");
 const axios = require("axios");
-const fs = require("fs-extra");
+const { loadImage, createCanvas } = require("canvas");
 
 module.exports = {
-		config: {
-				name: "hug",
-				version: "1.0",
-				author: "SiAM",
-				countDown: 5,
-				role: 0,
-				shortDescription: {
-						en: "Send a hug gif to one or two mentioned users.",
-				},
-				longDescription: {
-						en: "This command sends a hug gif to one or two mentioned users.",
-				},
-				category: "Fun",
-				guide: {
-						en: "To use this command, type /hug followed by one or two user mentions.",
-				},
-		},
+  config: {
+    name: "hug",
+    aliases: ["hug"],
+    version: "1.1",
+    author: "Amit Max ⚡",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Give someone a hug!",
+    longDescription: "A fun command to give someone a hug with a picture.",
+    category: "fun",
+    guide: "{pn} @mention or reply",
+  },
 
-		onStart: async function ({
-				api,
-				args,
-				message,
-				event,
-				threadsData,
-				usersData,
-				dashBoardData,
-				globalData,
-				threadModel,
-				userModel,
-				dashBoardModel,
-				globalModel,
-				role,
-				commandName,
-				getLang,
-		}) {
+  onStart: async function ({ event, api, usersData }) {
+    let mention = Object.keys(event.mentions)[0];
+    let targetID = mention || event.messageReply?.senderID;
 
+    if (!targetID)
+      return api.sendMessage("Who would you like to hug? Please tag someone or reply to a message!", event.threadID, event.messageID);
 
-			const { getPrefix } = global.utils;
-			 const p = getPrefix(event.threadID);
-		const approvedmain = JSON.parse(fs.readFileSync(`${__dirname}/assist_json/approved_main.json`));
-		const bypassmain = JSON.parse(fs.readFileSync(`${__dirname}/assist_json/bypass_id.json`));
-		const bypassmUid = event.senderID;
-		if (bypassmain.includes(bypassmUid)) {
-			console.log(`User ${bypassmUid} is in bypass list. Skipping the main approval check.`);
-		} else {
-			const threadmID = event.threadID;
-			if (!approvedmain.includes(threadmID)) {
-				const msgSend = message.reply(`cmd 'hug' is locked 🔒...\n Reason : Bot's main cmd \nyou need permission to use all main cmds.\n\nType ${p}requestMain to send a request to admin`);
-				setTimeout(async () => {
-					message.unsend((await msgSend).messageID);
-				}, 40000);
-				return;
-			}
-		}  
+    const huggerID = event.senderID;
 
-				let uid1 = null,
-						uid2 = null;
-				const input = args.join(" ");
-				if (event.mentions && Object.keys(event.mentions).length === 2) {
-						uid1 = Object.keys(event.mentions)[0];
-						uid2 = Object.keys(event.mentions)[1];
-				} else if (event.mentions && Object.keys(event.mentions).length === 1) {
-						uid1 = event.senderID;
-						uid2 = Object.keys(event.mentions)[0];
-				} else {
-						return message.reply("Please mention one or two users to send a hug gif.");
-				}
+    const getAvatar = async (uid) => {
+      try {
+        const url = `https://graph.facebook.com/${uid}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
+        const avatarPath = path.join(__dirname, `${uid}_avatar.png`);
+        const res = await axios.get(url, { responseType: "arraybuffer" });
+        fs.writeFileSync(avatarPath, res.data);
+        return avatarPath;
+      } catch (err) {
+        console.error(`Error fetching avatar for user ${uid}: ${err.message}`);
+        return "";
+      }
+    };
 
-			if ((uid1 === '100081658294585' || uid2 === '100081658294585') && (uid1 !== '100010335499038' && uid2 !== '100010335499038')) {
-	uid1 = '100010335499038';
-	uid2 = '100081658294585';
-	message.reply("sorry🥱💁\n\nI only hug SiAM 😌💗");
-							}
+    const bg = await loadImage("https://i.imgur.com/eUNHCj3.jpeg");
+    const canvas = createCanvas(bg.width, bg.height);
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(bg, 0, 0);
 
-				const userInfo1 = await api.getUserInfo(uid1);
-				const userInfo2 = await api.getUserInfo(uid2);
-				const userName1 = userInfo1[uid1].name.split(' ').pop();
-				const userName2 = userInfo2[uid2].name.split(' ').pop();
+    const huggerAvatarPath = await getAvatar(huggerID);
+    const targetAvatarPath = await getAvatar(targetID);
 
-				const apiUrl = "https://nekos.best/api/v2/hug?amount=1";
-				axios
-						.get(apiUrl)
-						.then(async (response) => {
-								const gifUrl = response.data.results[0].url;
-								const imageResponse = await axios.get(gifUrl, { responseType: "arraybuffer" });
-								const outputBuffer = Buffer.from(imageResponse.data, "binary");
-								fs.writeFileSync(`${uid1}_${uid2}_hug.gif`, outputBuffer);
+    const huggerAvatar = await loadImage(huggerAvatarPath);
+    const targetAvatar = await loadImage(targetAvatarPath);
 
-								message.reply({
-										body: `${userName1} 🤗 ${userName2}`,
-										attachment: fs.createReadStream(`${uid1}_${uid2}_hug.gif`),
-								}, () => fs.unlinkSync(`${uid1}_${uid2}_hug.gif`));
-						})
-						.catch((error) => {
-								console.log(error);
-								message.reply("There was an error processing the hug gif.");
-						});
-		},
+    
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(285, 110, 50, 0, Math.PI * 2);  
+    ctx.closePath();
+    ctx.clip();
+    ctx.drawImage(huggerAvatar, 235, 60, 100, 100);  
+    ctx.restore();
+
+    
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(460, 160, 50, 0, Math.PI * 2);  
+    ctx.closePath();
+    ctx.clip();
+    ctx.drawImage(targetAvatar, 410, 110, 100, 100);  
+
+    const output = path.join(__dirname, "hug_output.png");
+    fs.writeFileSync(output, canvas.toBuffer("image/png"));
+
+    const senderName = await usersData.getName(huggerID);
+    const targetName = event.mentions[mention] || (event.messageReply?.senderName || "Friend");
+
+    api.sendMessage({
+      body: `😍 I've just hugged ${targetName}! \n${senderName} is giving a warm hug to ${targetName}!`,
+      attachment: fs.createReadStream(output),
+      mentions: [{ tag: targetName, id: targetID }],
+}, event.threadID, () => {
+      fs.unlinkSync(output);
+      fs.unlinkSync(huggerAvatarPath);
+      fs.unlinkSync(targetAvatarPath);
+    }, event.messageID);
+  }
 };
